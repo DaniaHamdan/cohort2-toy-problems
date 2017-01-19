34c03d8ea2721bb1351afda38a27af27391433b5
DaniaHamdan <dandonzh@gmail.com>
2017-01-19 09:20:08 +0200

    linkedLIst

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -18,7 +18,45 @@
 
 
 var LinkedList = function(){
-  //fill me in!
+	this.head=null
+  	this.tail=null
+  
 };
+LinkedList.prototype.addToTail=function(value){
+	var node=Node(value)
+    if(!this.head){
+      this.head=node
+      this.tail=node
+    }else{
+     this.tail.next=node
+      this.tail=node
+    }
+    
+    return this.tail.value
+  };
+LinkedList.prototype.removeHead=function(){
+	var temp=this.head
+	this.head=this.head.next
+	return temp.value
+
+};
+
+LinkedList.prototype.contains=function(num){
+	var curent=this.head
+	while(curent){
+		if(curent.value===num){
+			return true
+		}
+		curent=curent.next
+	}
+	return false ;
+
+};
+function Node (value){
+return{
+	value:value,
+	next:null
+}
+}
 
 //write methods here!
