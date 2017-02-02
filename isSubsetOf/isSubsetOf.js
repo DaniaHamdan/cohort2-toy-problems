07d310d7dcaf1c4a49e1d0f85f3e6ada2976148d
DaniaHamdan <dandonzh@gmail.com>
2017-02-02 09:23:39 +0200

    extra credit

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -3,7 +3,6 @@
  * subset of an input array.  To simplify the problem, you can assume that neither
  * array will contain objects or arrays as elements within them.
  *
- *
  * var a = ['commit','push']
  * a.isSubsetOf(['commit','rebase','push','blame']) // true
  *
@@ -16,42 +15,25 @@
  * See http://en.wikipedia.org/wiki/Subset for more on the definition of a
  * subset.
  */
-
-
 /*
  * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.
  */
-
  Array.prototype.isSubsetOf = function (arr) {
- 	var array=[];
- 	var count=0;
+ 	var count=0
  	for (var i = 0; i < this.length; i++) {
- 		if(!array.includes(this[i])){
- 			array.push(this[i])
- 		}
- 	}
- 	for (var i = 0; i < array.length; i++) {
- 		if(arr.includes(array[i])){
+ 		if(arr.includes(this[i])){
  			count++
  		}
  	}
- 	if(count===array.length){
+ 	if(count===this.length){
  		return true;
  	}
  	return false 
  }
-
-
 // Extra credit
 Array.prototype.isSubsetOf = function (arr) {
-	var array=[];
  	var count=0;
- 	for (var i = 0; i < this.length; i++) {
- 		if(!array.includes(this[i])){
- 			array.push(this[i])
- 		}
- 	}
-	var str=JSON.stringify(array);
+	var str=JSON.stringify(this);
 	var str1=JSON.stringify(arr);
 	for (var i = 0; i < str.length; i++) {
  		if(str1.includes(str[i])){
